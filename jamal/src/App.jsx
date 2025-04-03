6320b60c198d7c6a6349cb464b06ee3d98fca2c7
ugrijaniStr <gospodaklosari11@gmail.com>
2025-04-03 21:06:52 +0200

    .

--- jamal/src/App.jsx
+++ jamal/src/App.jsx
@@ -1,9 +1,19 @@
-import './App.css'
+import './css/home.css';
+import './css/index.css';
 
 function App() {
   return (
     <>
-      
+      <div className="mainArea">
+        <div className="mainBox">
+          <div className="navbar">
+          </div>
+          <div className="parentBox">
+            <div className="childBox"></div>
+            <div className="childBox"></div>
+          </div>
+        </div>
+      </div>
     </> 
   )
 }
